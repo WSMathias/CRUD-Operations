7cc8465d6c4e04d669fdfe7ba62074815fc348de
WSMathias <wsmsamuel@gmail.com>
2017-09-01 22:39:17 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,225 @@
+var list =[];
+var GET="GET"
+var POST="POST"
+var UPDATE="PUT"
+var DELETE="DELETE"
+var inId=-1;
+var inName="";
+var inPrice=0;
+var inDes="";
+var ADD=false;
+var id =0;
+var count=0;
+
+$( document ).ready(function() {
+    loadItems();
+});
+
+function showFull(id){
+     loadFull(id)
+     document.getElementById("full-window").style.display = "block";
+     document.getElementById("fulldes").style.display= "block";
+}
+
+function done(){
+    if(!validateInput()){
+
+        alert("fiels can not be empty")
+    }
+
+    else{
+        if (ADD){
+            addData();
+        }
+        else{
+            id =parseInt(document.getElementById("in_id").value);
+            editData();
+        }
+        closeFull();
+        loadItems(); 
+    }
+}
+
+function closeFull(){
+    clear();
+    document.getElementById("fulldes").style.display= "none";
+    document.getElementById("editform").style.display ="none";
+    document.getElementById("full-window").style.display = "none";
+}
+
+function addbtn(){
+    editItem(-1);
+}
+
+function addData(){
+    var newItem={name:inName,price:inPrice,description:inDes};
+    Transfer(newItem,POST).then(
+        loadItems()
+    ,function (e) {
+        // handle errors
+    });
+}
+function editData(){
+    var newItem={id:inId,name:inName,price:inPrice,description:inDes};
+    Transfer(newItem,UPDATE).then(function(e){
+    loadItems();
+    },function (e) {
+    // handle errors
+    });
+}
+
+function deleteItem(id){
+    
+    closeFull()
+    Transfer(id,DELETE).then(function(e){
+        loadItems();
+    },function (e) {
+        // handle errors
+    });
+   
+    
+    loadItems();
+}
+function editItem(id=-1){
+    
+    showEdit();
+    ADD=true;
+    if(id>-1){
+        loadform(id);
+        ADD=false;
+    }
+
+}
+
+function validateInput(){
+    inId=document.getElementById("in_id").value;
+    inName=document.getElementById("in_name").value;
+    inPrice=parseInt(document.getElementById("in_price").value);
+    inDes=document.getElementById("in_des").value;
+    if(inName==""||inPrice==""||inPrice==NaN||inDes==""){
+        return false;
+    }
+    
+    return true;
+}
+function clear(){
+    document.getElementById("in_name").value="";
+    document.getElementById("in_price").value="";
+    document.getElementById("in_des").value="";
+}
+
+function showEdit(){
+    document.getElementById("fulldes").style.display= "none";
+    document.getElementById("full-window").style.display = "block";
+    document.getElementById("editform").style.display ="block";    
+}
+
+function loadform(id){
+    var ld=(list.filter(x => x.id === id))[0];
+    document.getElementById("in_id").value=ld.id;
+    document.getElementById("in_name").value=ld.name;
+    document.getElementById("in_price").value=ld.price;
+    document.getElementById("in_des").value=ld.description;
+}
+
+function loadFull(id){
+    var ld=(list.filter(x => x.id === id))[0];
+    var mytool=document.getElementById("tools-full").innerHTML=
+    `<a href="#" class="btn btn-warning btn-lg" onclick="editItem(`+id+`)"><span class="glyphicon glyphicon-pencil"></span> </a>
+    <a href="#" class="btn btn-danger btn-lg" onclick="deleteItem(`+id+`)"><span class="glyphicon glyphicon-trash"></span> </a>`
+    
+    document.getElementById("pop-name").textContent=ld.name;
+    document.getElementById("pop-price").textContent=ld.price;
+    document.getElementById("pop-des").textContent=ld.description;
+}
+
+function loadItems(){
+Transfer("",GET).then(function (data) {
+    list=data;
+    var mybody=document.getElementById("item_list");
+    var myNav=document.getElementById("nav-north");
+    mybody.innerHTML="";
+    myNav.innerHTML="";
+    for (i in list){
+        mybody.innerHTML+=`<div class="col-sm-3  col-xs-6 ibox"  >
+        <img src="images/birds.jpg" onclick="showFull(`+list[i].id+`)" class="img-responsive margin"  alt="Image">
+        <div class="ibox-text">
+        <h3>`+list[i].name+`</h3><h4>`+list[i].price+`</h4>
+        <p>`+list[i].description+`</p>
+        </div>
+        <div class="tools">
+            <a href="#" class="btn btn-warning btn-sm" onclick="editItem(`+list[i].id+`)"><span class="glyphicon glyphicon-pencil"></span> </a>
+            <a href="#" class="btn btn-danger btn-sm" onclick="deleteItem(`+list[i].id+`)"><span class="glyphicon glyphicon-trash"></span> </a>
+        </div>
+        </div>`
+        myNav.innerHTML+=`<li><a href="#">`+list[i].name+`</a></li>`
+
+    }
+    }, function (e) {
+      // handle errors
+    });
+
+}
+
+
+/******************************************************************
+ -----------------------Network Part-------------------------------
+ ******************************************************************/
+function Transfer(data,mode){
+    return new Promise(function (resolve, reject){
+    var id;
+    var dbAddr = " http://localhost:3000/items/";
+    console.log(data+" "+mode)
+    if (typeof data=="object"){
+        id=data.id;
+        delete data.id;
+    }
+    else{
+        id=data;
+        data=null;
+    }
+    data =JSON.stringify(data);
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;  
+    switch (mode){
+        case GET   : dbAddr=dbAddr;break;
+        case POST  : dbAddr=dbAddr;break;
+        case UPDATE: dbAddr=dbAddr+id;break;
+        case DELETE: dbAddr=dbAddr+id;break;
+    }
+    switch (mode){
+        case GET   :
+         xhr.open("GET", dbAddr);
+         xhr.setRequestHeader("content-type", "application/json");        
+         xhr.setRequestHeader("cache-control", "no-cache");
+         xhr.onload = () => resolve(JSON.parse(xhr.responseText));
+         xhr.onerror = () => reject(xhr.statusText);
+         xhr.send(data);
+        break;
+        case POST  :
+         xhr.open("POST", dbAddr);
+         xhr.setRequestHeader("content-type", "application/json");
+         xhr.setRequestHeader("cache-control", "no-cache");        
+         xhr.onload = () => resolve(xhr.responseText);
+         xhr.onerror = () => reject(xhr.statusText)
+         xhr.send(data);
+        break;
+        case UPDATE:
+         xhr.open("PUT", dbAddr);
+         xhr.setRequestHeader("content-type", "application/json");
+         xhr.setRequestHeader("cache-control", "no-cache");
+         xhr.onload = () => resolve(xhr.responseText);
+         xhr.onerror = () => reject(xhr.statusText)
+         xhr.send(data);
+        break;      
+        case DELETE:     
+         xhr.open("DELETE", dbAddr);
+         xhr.setRequestHeader("cache-control", "no-cache");
+         xhr.onload = () => resolve(xhr.responseText);
+         xhr.onerror = () => reject(xhr.statusText)
+         xhr.send(data);
+        break;
+    }
+});
+}
+/**************************************************************** */
